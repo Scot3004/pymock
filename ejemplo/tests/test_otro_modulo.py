14bdf5525980698f775b5fe3ca098337cab44824
Sergio C. Orozco Torres <sergio@joonik.com>
2017-01-30 18:56:35 -0500

    Ejemplos sentinel

--- ejemplo/tests/test_otro_modulo.py
+++ ejemplo/tests/test_otro_modulo.py
@@ -1,6 +1,6 @@
 import unittest
-from mock import patch, Mock
-from mi_paquete.otro_modulo import saludo_foraneo
+from mock import patch, Mock, sentinel
+from mi_paquete.otro_modulo import saludo_foraneo, soy_vago
 
 class SaludoOriginalTestCase(unittest.TestCase):
     def test_saludo(self):
@@ -12,3 +12,28 @@ class SaludoOriginalTestCase(unittest.TestCase):
         mock_saludo.return_value = 'cambie la salida'
         self.assertEqual('cambie la salida',
         saludo_foraneo())
+
+    @patch('mi_paquete.otro_modulo.Original.saludo')
+    def test_mock_sentinel_saludo(self, mock_saludo):
+        mock_saludo.return_value = sentinel.salida
+        self.assertEqual(sentinel.salida,
+        saludo_foraneo())
+
+    @patch('mi_paquete.otro_modulo.Original.saludo')
+    def test_mock_raise_saludo(self, mock_saludo):
+        mock_saludo.side_effect = ValueError('Lo hice reventar')
+        self.assertRaises(ValueError)
+
+    @patch('mi_paquete.otro_modulo.Original.saludo')
+    def test_mock_multiple_saludo(self, mock_saludo):
+        mock_saludo.side_effect = [sentinel.salida1, sentinel.salida2]
+        self.assertEqual(sentinel.salida1,
+        saludo_foraneo())
+        self.assertEqual(sentinel.salida2,
+        saludo_foraneo())
+
+    @patch('mi_paquete.otro_modulo.Original.saludo')
+    def test_mock_call_saludo(self, mock_saludo):
+        saludo_foraneo()
+        mock_saludo.assert_called_with('Pythonist')
+
